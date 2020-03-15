3192923b73ee7b9a499bb3e02572a65ed4fc7db8
Saajidh Umar <saajidhumar@gmail.com>
2020-03-15 15:42:32 +0530

    logout and loggedin method,conditional repesenting

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,13 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor() { }
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('logged in succesfully');
+    }, error => {
+      console.log('failed to login');
+    });
+  }
+
+  loggedin() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    console.log('logged out');
   }
 
 }
