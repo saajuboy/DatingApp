459fb2a923fff13e0b1680b8a4b2f02bc1e82cc6
Saajidh Umar <saajidhumar@gmail.com>
2020-03-22 23:20:41 +0530

    Using and Configuring NG Jwt

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -25,8 +25,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedin() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
