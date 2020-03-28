2c09ff77bd1d5dec5f27f693061d0cf3a9ea08c8
Saajidh Umar <saajidhumar@gmail.com>
2020-03-28 10:55:50 +0530

    Added interface for user and photo in typescript

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,19 @@
+import { Photo } from './Photo';
+
+export interface User {
+    id: number;
+    username: string;
+    KnownAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+
+    interests?: string;
+    introduction?: string;
+    lokkingFor?: string;
+    photos?: Photo[];
+}
