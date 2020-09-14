bd702630a42a77e4513f1bf75c4ae3d0f5aff471
Saajidh Umar <saajidhumar@gmail.com>
2020-09-15 00:36:42 +0530

    setMainPhoto func in SPA

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Photo } from 'src/app/_models/Photo';
 import { FileUploader } from 'ng2-file-upload';
 import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -11,39 +13,14 @@ import { AuthService } from 'src/app/_services/auth.service';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
+  @Output() getMemberPhotoChange = new EventEmitter<string>();
 
   uploader: FileUploader;
   hasBaseDropZoneOver: boolean;
   baseUrl = environment.apiUrl;
-  response: string;
+  currentMainPhoto: Photo;
 
-  // constructor() {
-  //   this.uploader = new FileUploader({
-  //     url: URL,
-  //     disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
-  //     formatDataFunctionIsAsync: true,
-  //     formatDataFunction: async (item) => {
-  //       return new Promise((resolve, reject) => {
-  //         resolve({
-  //           name: item._file.name,
-  //           length: item._file.size,
-  //           contentType: item._file.type,
-  //           date: new Date()
-  //         });
-  //       });
-  //     }
-  //   });
-
-  //   this.hasBaseDropZoneOver = false;
-  //   this.hasAnotherDropZoneOver = false;
-
-  //   this.response = '';
-
-  //   this.uploader.response.subscribe(res => this.response = res);
-  // }
-
-
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private userService: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.initializeUploader();
@@ -79,5 +56,16 @@ export class PhotoEditorComponent implements OnInit {
         this.photos.push(photo);
       }
     };
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
+      this.currentMainPhoto = this.photos.filter(a => a.isMain === true)[0];
+      this.currentMainPhoto.isMain = false;
+      photo.isMain = true;
+      this.getMemberPhotoChange.emit(photo.url);
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 }
