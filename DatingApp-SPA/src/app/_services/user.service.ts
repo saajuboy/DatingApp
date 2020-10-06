eccc44246a6927926becb2ad095cccc060dffa79
Saajidh Umar <saajidhumar@gmail.com>
2020-10-06 23:28:42 +0530

    Additional filter feature for memberslist

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -21,7 +21,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(page?, itemsPerPage?): Observable<PaginatedResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
 
     // tslint:disable-next-line: prefer-const
@@ -32,6 +32,11 @@ export class UserService {
       params = params.append('pageSize', itemsPerPage);
     }
 
+    if (userParams != null) {
+      params = params.append('minAge', userParams.minAge);
+      params = params.append('maxAge', userParams.maxAge);
+      params = params.append('gender', userParams.gender);
+    }
     return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params })
       .pipe(
         map(response => {
--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -11,5 +11,10 @@ namespace DatingApp.API.Helpers
             set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
         }
 
+        public int UserId { get; set; }
+        public string Gender { get; set; }
+
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
     }
 }
